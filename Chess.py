471bd4fa7fd05903774a57c18469ba8a8a63a89f
Sawvtoj <73905006+Sawvtoj@users.noreply.github.com>
2021-02-10 10:01:11 -0600

    Making and undo moves
    Figuring out how to make a move and undo a move

--- Chess.py
+++ Chess.py
@@ -17,19 +17,62 @@ def load_Images():
         IMAGES[piece] = p.transform.scale(p.image.load('ChessImages/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
         
 def main():
+    
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color('White'))
+    
     gs = Engine.Brain()
+    valid_Moves = gs.get_Valid_Moves()
+    move_Made = False
+    
     load_Images()
+    
+    sq_Selected  = () #keeps track of the current selected square
+    player_Clicks = [] #keeps track of where the user is clicking with the mouse button
+
     running = True
     while running:
         for e in p.event.get():
+            
             if e.type == p.QUIT:
                 running = False
                 p.quit()
-        
+            
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x, y) location of the mouse
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                
+                if sq_Selected == (row, col): #if the user selected the same square
+                    sq_Selected = () #reset
+                    player_Clicks = [] #reset
+                
+                else:
+                    sq_Selected = (row, col)
+                    player_Clicks.append(sq_Selected)
+                
+                if len(player_Clicks) == 2:
+                    move = Engine.Move(player_Clicks[0], player_Clicks[1], gs.board)
+                    print(move.get_Chess_Notation())
+                    
+                    if move in valid_Moves:
+                        gs.make_Move(move)
+                        move_Made = True
+                    
+                    sq_Selected = ()
+                    player_Clicks = []
+                   
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undo_Move()
+                    move_Made = True
+                    
+        if move_Made:
+            valid_Moves = gs.get_Valid_Moves()
+            move_Made = False
+            
         draw_Game_State(screen, gs)        
                 
         clock.tick(MAX_FPS)
--- Engine.py
+++ Engine.py
@@ -31,3 +31,83 @@ class Brain():
         
         self.white_Turn = True
         self.move_Log = []
+        
+    def make_Move(self, move):
+        self.board[move.start_Row][move.start_Col] = '  '
+        self.board[move.end_Row][move.end_Col] = move.piece_Moved
+        self.move_Log.append(move)
+        self.white_Turn = not self.white_Turn #change turns 
+
+    def undo_Move(self):
+        if len(self.move_Log) != 0:
+            move = self.move_Log.pop()
+            self.board[move.start_Row][move.start_Col] = move.piece_Moved
+            self.board[move.end_Row][move.end_Col] = move.piece_Captured
+            self.white_Turn = not self.white_Turn
+            
+    def get_Valid_Moves(self):
+        return self.get_All_Possible_Moves()
+    
+    def get_All_Possible_Moves(self): 
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'W' and self.white_Turn) or (turn == 'B' and not self.white_Turn):
+                    piece = self.board[r][c][1]
+                    if piece == 'P':
+                        self.get_Pawn_Moves(r, c, moves)
+                    elif piece == 'R':
+                        self.get_Rook_Moves(r, c, moves)
+        return moves
+
+    def get_Pawn_Moves(self, r, c, moves):
+        pass
+        
+    def get_Rook_Moves(self, r, c, moves):
+        pass
+    
+    def get_Bishop_Moves(self, r, c, moves):
+        pass
+    
+    def get_Knight_Moves(self, r, c, moves):
+        pass
+    
+    def get_Queen_Moves(self, r, c, moves):
+        pass
+    
+    def get_King_Moves(self, r, c, moves):
+        pass    
+#####################################################################        
+#####################################################################
+##################################################################### 
+
+       
+class Move():
+    
+    ranks_To_Rows = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+    rows_To_Ranks = {v: k for k, v in ranks_To_Rows.items()}
+    files_To_Cols = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    cols_To_Files = {v: k for k, v in files_To_Cols.items()}
+    
+    def __init__(self, start_Sq, end_Sq, board):
+        self.start_Row = start_Sq[0]
+        self.start_Col = start_Sq[1]
+        self.end_Row = end_Sq[0]
+        self.end_Col = end_Sq[1]
+        self.piece_Moved = board[self.start_Row][self.start_Col]
+        self.piece_Captured = board[self.end_Row][self.end_Col]
+        self.move_ID = self.start_Row * 1000 + self.start_Col * 100 + self.end_Row * 10 + self.end_Col
+      
+    #Overriding the equals method    
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.move_ID == other.move_ID
+        return False
+        
+    def get_Chess_Notation(self):
+        #Could change to real chess notation
+        return self.get_Rank_File(self.start_Row, self.start_Col) + self.get_Rank_File(self.end_Row, self.end_Col)
+        
+    def get_Rank_File(self, r, c):
+        return self.cols_To_Files[c] + self.rows_To_Ranks[r]
